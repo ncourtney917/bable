2bc5ac1a6b93c878c6491a645214c77c39f2eb05
nc2 <circlethewagons21@protonmail.com>
2022-03-26 17:01:12 -0400

    Add keyboard functionality

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ export const AppContext = createContext();
 
 function App() {
   const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
   return (
     <div className="App">
       <nav>
         <h1>Bable</h1>
       </nav>
-      <AppContext.Provider value={{ board, setBoard }}>
-        <Board />
-        <Keyboard />
+      <AppContext.Provider value={{ board, setBoard, currAttempt, setCurrAttempt }}>
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
       </AppContext.Provider>
     </div>
   );
