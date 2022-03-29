9eb772d13faddb90cbbca981fa346962ad57c0b8
nc2 <circlethewagons21@protonmail.com>
2022-03-28 23:30:56 -0400

    Bug fix: complex logic for yellow letters

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -4,12 +4,27 @@ import { AppContext } from "../App";
 function Letter({ letterPos, attemptVal }) {
     const { board, correctWord, currAttempt, setDisabledLetters } = useContext(AppContext);
     const letter = board[attemptVal][letterPos];
+    const guess = board[attemptVal].slice(0, correctWord.length)
 
+    // Right letter in right spot
     const correct = correctWord[letterPos] === letter
-    const almost = !correct && letter !== "" && correctWord.includes(letter)
+
+    // Remove correctly guessed letters from correctWord
+    var remainingLetters = ""
+    for (var i = 0; i < correctWord.length; i++) {
+        if (correctWord[i] !== guess[i]) {
+            remainingLetters = remainingLetters + correctWord[i];
+        }
+    }
+    // Remove letters to the left of the current letter that are in correctWord
+    var prevLetters = guess.slice(0, letterPos);
+    var re = new RegExp(`[${prevLetters}]`, "g");
+    var almostSearch = remainingLetters.replace(re, '');
+
+    // Right letter in wrong spot
+    const almost = !correct && letter !== "" && almostSearch.includes(letter)
     const letterState = currAttempt.attempt > attemptVal &&
         (correct ? "correct" : almost ? "almost" : "error");
-
 
     useEffect(() => {
         if (letter !== "" && !correct && !almost) {
