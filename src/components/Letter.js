a7846f22b4a9619b632e9e04597e5ac41ac03124
nc2 <circlethewagons21@protonmail.com>
2022-04-03 17:20:02 -0400

    Fix bug in calculating 'almost' characters

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -5,30 +5,42 @@ function Letter({ letterPos, attemptVal }) {
     const { board, correctWord, currAttempt, setDisabledLetters, letterCount } = useContext(AppContext);
     const letter = board[attemptVal][letterPos];
     const guess = board[attemptVal].slice(0, correctWord.length)
+    var correct, almost = false
 
-    // Right letter in right spot
-    const correct = correctWord[letterPos] === letter
-
-    // Check if letter is 'almost' correct => Right letter, wrong spot
-    // Should only be 'almost' if this is the first instance (furthest to left) of letter
-    // Should NOT be 'almost' if this letter is 'correct' somewhere else in the word
-    var remainingLetters = ""
     if (letter !== "") {
-        // Remove correctly guessed letters from correctWord
-        for (var i = 0; i < letterCount; i++) {
-            if (correctWord[i] !== guess[i]) {
-                remainingLetters = remainingLetters + correctWord[i];
+        if (correctWord[letterPos] === letter) {
+            // Is letter "correct" (Right letter in right spot)
+            correct = true
+        } else {
+            // Is letter "almost" correct (Right letter, wrong spot)
+            // Should NOT be "almost" if this letter is 'correct' somewhere else in the word
+            // Should only be "almost" if this is the first instance (furthest to left) of letter not in correct spot
+
+            // correctRemaining is the correctWord, with correctLetters removed
+            // guessRemaining is the guessWord with correctLetters replaced with "-"
+            var correctRemaining = ""
+            var guessRemaining = guess
+
+            // Find letters that were already correctly guessed (correctLetters)
+            for (var i = 0; i < letterCount; i++) {
+                if (correctWord[i] !== guess[i]) {
+                    correctRemaining = correctRemaining + correctWord[i];
+                } else {
+                    guessRemaining[i] = "-"
+                };
             }
-        }
-        // Remove letters to the left of the current letter that are in correctWord
-        var prevLetters = guess.slice(0, letterPos);
-        var re = new RegExp(`[${prevLetters}]`, "g");
-        // FOUND BUG HERE WHEN GUESSING WORD NNNNNICHOLAS. REPLACE WILL REPLACE ALL THE INSTANCES OF N, NOT JUST 1.
-        //TODO 
-        remainingLetters = remainingLetters.replace(re, '');
+
+            // Remove letters to the left of the current letter that are in correctWord in a different position
+            var prevLetters = guessRemaining.slice(0, letterPos);
+            for (var j = 0; j < prevLetters.length; j++) {
+                correctRemaining = correctRemaining.replace(prevLetters[j], '');
+            }
+
+            // Is letter in the remaining letters of the correctWord?
+            almost = correctRemaining.includes(letter)
+        };
     };
-    // Right letter in wrong spot
-    const almost = !correct && remainingLetters.includes(letter)
+
     const letterState = currAttempt.attempt > attemptVal &&
         (correct ? "correct" : almost ? "almost" : "error");
 
