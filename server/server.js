96161c5b1d299400518f27326a075d91b3b3a96b
nc2 <circlethewagons21@protonmail.com>
2022-07-11 16:26:42 -0400

    Use default express routing

--- server/server.js
+++ server/server.js
@@ -3,12 +3,11 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 console.log(__dirname)
-const publicPath = path.join(__dirname, '..', 'build');
 
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
  });
  
 app.listen(port, () => {
