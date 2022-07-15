3e9059c398758c080e547259875fa4791213b429
nc2 <circlethewagons21@protonmail.com>
2022-07-14 22:19:39 -0400

    More mobile styling

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,25 +6,27 @@ function Board() {
     const { letterCount } = useContext(AppContext);
     const data = Array.from(Array(letterCount).keys());
     return (
-        <div className="board">
-            {" "}
-            <div className="row">
-                {data.map(i => <Letter letterPos={i} attemptVal={0} />)}
-            </div>
-            <div className="row">
-                {data.map(i => <Letter letterPos={i} attemptVal={1} />)}
-            </div>
-            <div className="row">
-                {data.map(i => <Letter letterPos={i} attemptVal={2} />)}
-            </div>
-            <div className="row">
-                {data.map(i => <Letter letterPos={i} attemptVal={3} />)}
-            </div>
-            <div className="row">
-                {data.map(i => <Letter letterPos={i} attemptVal={4} />)}
-            </div>
-            <div className="row">
-                {data.map(i => <Letter letterPos={i} attemptVal={5} />)}
+        <div className="gameArea">
+            <div className="board">
+                {" "}
+                <div className="row">
+                    {data.map(i => <Letter letterPos={i} attemptVal={0} />)}
+                </div>
+                <div className="row">
+                    {data.map(i => <Letter letterPos={i} attemptVal={1} />)}
+                </div>
+                <div className="row">
+                    {data.map(i => <Letter letterPos={i} attemptVal={2} />)}
+                </div>
+                <div className="row">
+                    {data.map(i => <Letter letterPos={i} attemptVal={3} />)}
+                </div>
+                <div className="row">
+                    {data.map(i => <Letter letterPos={i} attemptVal={4} />)}
+                </div>
+                <div className="row">
+                    {data.map(i => <Letter letterPos={i} attemptVal={5} />)}
+                </div>
             </div>
         </div>
     );
