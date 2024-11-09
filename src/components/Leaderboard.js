b6f7ccfbc6b20309e89e7ff39223a0462986d463
NC <nick.courtney22@gmail.com>
2024-11-09 17:54:38 -0500

    Init leaderboard component

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,25 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { AppContext } from '../pages/playgame';
+import Confetti from 'react-confetti';
+import {useParams} from 'react-router-dom';
+import Popup from 'reactjs-popup';
+
+
+function Leaderboard() {
+
+
+    // Create screen for winners and losers
+    return (
+        <div>
+            <h1>Leaderboard</h1>
+        </div>
+    )
+}
+
+
+export default Leaderboard
+
+
+
+
+
