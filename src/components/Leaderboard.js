69830cd359ae5dd04277bfb0454cdba9d0925e2a
NC <nick.courtney22@gmail.com>
2024-11-16 23:37:48 -0500

    Move leaderboard styling to css

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -22,30 +22,7 @@ function Leaderboard () {
             modal={true}
             closeOnDocumentClick={false}
             closeOnEscape={false}
-            overlayStyle={{
-                background: 'rgba(0, 0, 0, 0.7)', // Optional, to make the overlay darkened
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-              }}
-              contentStyle={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                width: '100vw',
-                height: '100vh',
-                padding: '0', // Remove any default padding
-                margin: '0', // Remove any default margin
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: '#fff',
-                zIndex: 1000,
-              }}
+            className="leaderboard-overlay"            
         >
             {(close) => (
                 <div className="gameOver popup-leaderboard">
